1d68141f2caa2f02b1caff6d87473e51e1308e9f
pengchongfu <pengchongfu@gmail.com>
2017-05-30 21:21:11 +0800

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'nuxt-blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -77,5 +77,8 @@ module.exports = {
       callback(null, routes)
     },
     interval: 1
+  },
+  router: {
+    base: '/nuxt-blog/'
   }
 }
